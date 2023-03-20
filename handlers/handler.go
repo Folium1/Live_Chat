471c402c2a07e4393a3ebc99bf884c518454be6a
Folium1 <vampirowitch.vampir@gmail.com>
2023-03-20 08:38:20 +0200

    Created handler for index page, and deleted mock chat handler

--- handlers/handler.go
+++ handlers/handler.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"fmt"
 	"html/template"
 
 	auth "chat/handlers/middleware"
@@ -14,37 +13,6 @@ import (
 var (
 	Tmpl *template.Template
 )
-
-func mockChat() http.Handler {
-	funcName := logger.GetFuncName()
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		logger.InfoHttp(r.URL.Path, r.Method, funcName)
-		token, err := auth.GetToken(r)
-		if err != nil {
-			logger.Error("Couldn't get token from cookies", err, funcName)
-			http.Redirect(w, r, "/login", http.StatusNotFound)
-			return
-		}
-		userId, err := auth.ValidateToken(token)
-		if err != nil {
-			logger.Error("Couldn't validate token", err, funcName)
-			http.Redirect(w, r, "/login", http.StatusNotFound)
-			return
-		}
-		user, err := userController.GetUserById(userId)
-		if err != nil {
-			logger.Error(fmt.Sprintf("coudn't get user by id(%v)", userId), err, funcName)
-			http.Error(w, "couldn't get user data", 404)
-			return
-		}
-		msg := fmt.Sprintf("Hello,%v!!!", user.Name)
-		_, err = w.Write([]byte(msg))
-		if err != nil {
-			logger.Error("couldn't write message to user", err, funcName)
-		}
-	})
-
-}
 
 func StartServer() {
 	var err error
