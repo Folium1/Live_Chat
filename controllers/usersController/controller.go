a180d2ba72cb14e608373cd5f34c5120608bc5ed
Folium1 <vampirowitch.vampir@gmail.com>
2023-03-21 11:59:03 +0200

    Added new func for getting user by his id

--- controllers/usersController/controller.go
+++ controllers/usersController/controller.go
@@ -19,6 +19,7 @@ func New(msg user.UserService) UserController {
 type UserController interface {
 	CreateUser(newUser dto.CreateUserDTO) error
 	GetUser(userData dto.GetUserDTO) (dto.GetUserDTO, error)
+	GetUserById(id int) (dto.ChatUserDTO, error)
 }
 
 func (c *userController) CreateUser(newUser dto.CreateUserDTO) error {
@@ -40,7 +41,7 @@ func (c *userController) GetUser(userData dto.GetUserDTO) (dto.GetUserDTO, error
 	if err != nil {
 		log.Printf("couldn't parse dto data to db struct, err: %v", err)
 	}
-	userFromDb, err := c.db.GetUser(dbUser)
+	userFromDb, err := c.db.UserByMail(dbUser)
 	if err != nil {
 		log.Printf("couldn't get user, err: %v", err)
 		return dto.GetUserDTO{}, err
@@ -50,6 +51,21 @@ func (c *userController) GetUser(userData dto.GetUserDTO) (dto.GetUserDTO, error
 	if err != nil {
 		log.Printf("couldn't parse db data to dto struct, err: %v", err)
 		return dto.GetUserDTO{}, err
+	}
+	return userDTO, nil
+}
+
+func (c *userController) GetUserById(id int) (dto.ChatUserDTO, error) {
+	user, err := c.db.GetUserById(id)
+	if err != nil {
+
+		return dto.ChatUserDTO{}, err
+	}
+	var userDTO dto.ChatUserDTO
+	err = parser.ParseToDTO(user, &userDTO)
+	if err != nil {
+		log.Printf("Couldn't parse db data to dto, err: %v", err)
+		return dto.ChatUserDTO{}, err
 	}
 	return userDTO, nil
 }
