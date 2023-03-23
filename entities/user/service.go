a80f6b9e81a918223eee9c03e892c69873efaf3b
Folium1 <vampirowitch.vampir@gmail.com>
2023-03-23 23:04:17 +0200

    Added GetUserById function

--- entities/user/service.go
+++ entities/user/service.go
@@ -1,23 +1,23 @@
 package user
 
 import (
-	entity "chat/entities"
 	"fmt"
 	"log"
-)
 
-const userTable = "users"
+	entity "chat/entities"
+)
 
 type UserService interface {
 	CreateUser(newUser User) error
-	GetUser(user User) (User, error)
+	UserByMail(user User) (User, error)
+	GetUserById(id int) (User, error)
 }
 
 func New() UserService {
 	return &User{}
 }
 
-// Creats new User
+// CreateUser Creats new User
 func (u *User) CreateUser(newUser User) error {
 	db, err := entity.DbConnect()
 	if err != nil {
@@ -33,26 +33,51 @@ func (u *User) CreateUser(newUser User) error {
 	return nil
 }
 
-// Gets user by password and mail
-func (u *User) GetUser(user User) (User, error) {
+// UserByMail returns user by his mail
+func (u *User) UserByMail(user User) (User, error) {
 	db, err := entity.DbConnect()
 	if err != nil {
 		log.Panicf("Couldn't connect to db, err: %v", err)
 	}
 	defer db.Close()
 	query := fmt.Sprintf("SELECT id,password FROM chat.users WHERE email = '%v';", user.Mail)
-	q, err := db.Query(query)
+	raws, err := db.Query(query)
 	if err != nil {
 		err = fmt.Errorf("Couldn't get user, err: %v", err)
-		log.Printf(err.Error())
+		log.Println(err)
 		return User{}, err
 	}
-	for q.Next() {
-		err := q.Scan(&user.Id, &user.Password)
+	for raws.Next() {
+		err := raws.Scan(&user.Id, &user.Password)
 		if err != nil {
 			log.Printf("Couldn't parse user's id, err: %v", err)
 		}
 	}
 	return user, nil
 
+}
+
+// GetUserById returns user by his шd
+func (u *User) GetUserById(id int) (User, error) {
+	db, err := entity.DbConnect()
+	if err != nil {
+		log.Panicf("Couldn't connect to db, err: %v", err)
+	}
+	defer db.Close()
+	query := fmt.Sprintf("SELECT name,email FROM chat.users WHERE id = %v;", id)
+	raws, err := db.Query(query)
+	if err != nil {
+		log.Printf("No user with id %v", id)
+		return User{}, err
+	}
+	var user User
+	user.Id = id
+	for raws.Next() {
+		err = raws.Scan(&user.Name, &user.Mail)
+		if err != nil {
+			log.Printf("Couldn't parse data to user struct, err:%v", err)
+			return User{}, err
+		}
+	}
+	return user, err
 }
