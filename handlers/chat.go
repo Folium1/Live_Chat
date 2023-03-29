ce19c0774cf3f883e7d89e0271d2d1ba06e63c39
Folium1 <vampirowitch.vampir@gmail.com>
2023-03-29 20:06:16 +0300

    Added deleteMessage function and refactored chat function

--- handlers/chat.go
+++ handlers/chat.go
@@ -2,8 +2,10 @@ package handlers
 
 import (
 	chatdto "chat/DTO/chatDTO"
+	chatDb "chat/entities/chat"
+	"chat/controllers/chatController"
 	auth "chat/handlers/middleware"
-	logger "chat/logger"
+	"chat/logger"
 	"encoding/json"
 	"strings"
 
@@ -18,20 +20,21 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 var (
-	clients []websocket.Conn
+	clients           []websocket.Conn
+	messageService    = chatDb.Message{}
+	messageController = chatController.New(&messageService)
 )
 
 // chatTemplate executes html template
 func chatTemplate(w http.ResponseWriter, r *http.Request) {
 	funcName := logger.GetFuncName()
-	_, err := auth.GetToken(r)
+	_, err := auth.GetToken(w, r)
 	if err != nil {
-		auth.DeleteCookies(w)
-		http.Redirect(w, r, "/login", 302)
+		handleAuthError(w, r, funcName)
 		return
 	}
 	// loading history of previous massages
-	prevMessages, err := chController.GetAllMessages()
+	prevMessages, err := messageController.GetAllMessages()
 	if err != nil {
 		logger.Error("Couldn't get all messages from db", err, funcName)
 		http.Error(w, "Server error", 500)
@@ -41,31 +44,34 @@ func chatTemplate(w http.ResponseWriter, r *http.Request) {
 }
 
 func chat(w http.ResponseWriter, r *http.Request) {
+	// Log function name and HTTP request details
 	funcName := logger.GetFuncName()
 	logger.InfoHttp(r.URL.Path, r.Method, funcName)
-	// getting token from user's cookies
-	token, err := auth.GetToken(r)
+
+	// Get user token from cookies
+	token, err := auth.GetToken(w, r)
 	if err != nil {
-		auth.DeleteCookies(w)
-		http.Redirect(w, r, "/login/", 302)
+		handleAuthError(w, r, funcName)
 		return
 	}
-	// validate token and getting user's id
+
+	// Validate user token and get user ID
 	userId, err := auth.ValidateToken(token)
 	if err != nil {
-		auth.DeleteCookies(w)
+		handleAuthError(w, r, funcName)
 		logger.Error("Couldn't validate token", err, funcName)
-		http.Redirect(w, r, "/login/", 302)
 		return
 	}
-	// getting user's information from db by his id
+
+	// Get user information from DB by user ID
 	user, err := userController.GetUserById(userId)
 	if err != nil {
-		auth.DeleteCookies(w)
-		logger.Error(fmt.Sprintf("Couldn't get user by id(%v)", userId), err, funcName)
-		http.Redirect(w, r, "/login/", 302)
+		handleAuthError(w, r, funcName)
+		logger.Error(fmt.Sprintf("Couldn't get user by ID (%v)", userId), err, funcName)
 		return
 	}
+
+	// Upgrade connection to WebSocket
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		logger.Error("Couldn't upgrade connection to WebSocket", err, funcName)
@@ -73,44 +79,67 @@ func chat(w http.ResponseWriter, r *http.Request) {
 	}
 	defer conn.Close()
 
-	clients = append(clients, *conn)
+	// Add new client to clients slice
+	addClient(conn)
 
+	// Read messages from client
 	for {
 		msgType, msg, err := conn.ReadMessage()
 		if err != nil {
 			return
 		}
-		message := chatdto.CreateMessageDTO{}
-		message.Text = string(msg)
-		message.UserId = userId
-		message.UserName = user.Name
-		chController.CreateMsg(message)
-		for _, client := range clients {
-			err := client.WriteMessage(msgType, []byte(message.UserName+": "+message.Text))
-			_, err = auth.GetToken(r)
-			if err != nil {
-
-			}
-			if err != nil {
-				logger.Error("Couldn't deliver msg", err, funcName)
-			}
+		message := chatdto.CreateMessageDTO{
+			Text:     string(msg),
+			UserId:   userId,
+			UserName: user.Name,
+		}
+		if err := createMessageAndBroadcast(message, msgType); err != nil {
+			logger.Error("Couldn't create message", err, funcName)
+			http.Error(w, "Server error", 500)
+			return
 		}
 	}
+}
+
+func handleAuthError(w http.ResponseWriter, r *http.Request, funcName string) {
+	auth.DeleteCookies(w)
+	http.Redirect(w, r, "/login", 302)
+}
+
+func addClient(conn *websocket.Conn) {
+	clients = append(clients, *conn)
+}
+
+func createMessageAndBroadcast(msg chatdto.CreateMessageDTO, msgType int) error {
+	var err error
+	msg.Id, err = messageController.CreateMsg(msg)
+	if err != nil {
+		return err
+	}
+	newMessageData, _ := json.Marshal(msg)
+	for _, client := range clients {
+		if err := client.WriteMessage(msgType, newMessageData); err != nil {
+			logger.Error("Couldn't deliver message", err, logger.GetFuncName())
+		}
+	}
+	return nil
 }
 
 // make a handle function to delete message by id from db
 func deleteMessage(w http.ResponseWriter, r *http.Request) {
 	funcName := logger.GetFuncName()
-	path := r.URL.Path
-	parts := strings.Split(path, "/")
-	id := parts[2]
-	fmt.Println(id)
+	logger.InfoHttp(r.URL.Path, r.Method, funcName)
+
+	// Get the ID of the message to be deleted from the URL path
+	id := strings.Split(r.URL.Path, "/")[2]
 	if id == "" {
-		logger.Error("Couldn't delete message", fmt.Errorf("No message id provided"), funcName)
+		logger.Error("No message id provided", fmt.Errorf("No message id provided"), funcName)
 		http.Error(w, "Server error", 500)
 		return
 	}
-	err := chController.DeleteMsg(id)
+
+	// Delete the message from the database
+	err := messageController.DeleteMsg(id)
 	if err != nil {
 		logger.Error("Couldn't delete message", err, funcName)
 		http.Error(w, "Server error", 500)
