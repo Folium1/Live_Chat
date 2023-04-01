82869bf229b426448bce8d8026b799342bad93e2
Folium1 <vampirowitch.vampir@gmail.com>
2023-04-01 22:16:08 +0300

    Deleted 'created_at' and 'updated_at' fields from message struct

--- entities/chat/Model.go
+++ entities/chat/Model.go
@@ -2,9 +2,7 @@ package chat
 
 type Message struct {
 	Id        int    `json:"messageId"`
-	UserId    int    `json:"userId"`
+	UserId    string `json:"userId"`
 	UserName  string `json:"user_name"`
 	Text      string `json:"text"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
 }
