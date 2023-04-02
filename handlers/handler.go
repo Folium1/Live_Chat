628a6b91a51ad9748a3fb24387d8616c8e8e7076
Folium1 <vampirowitch.vampir@gmail.com>
2023-04-02 23:07:05 +0300

    Added /logout/ handler

--- handlers/handler.go
+++ handlers/handler.go
@@ -2,27 +2,26 @@ package handlers
 
 import (
 	middleware "chat/handlers/middleware"
+	templateparser "chat/handlers/templateParser"
+	userHandler "chat/handlers/user"
+	socket "chat/handlers/websocket"
 	logger "chat/logger"
-	"html/template"
+
 	"net/http"
 )
 
 var (
-	Tmpl *template.Template
+	tmpl = templateparser.TemplateParse()
 )
 
 func StartServer() {
 	var err error
-	Tmpl, err = template.ParseGlob("templates/*.html")
-	if err != nil {
-		funcName := logger.GetFuncName()
-		logger.Error("Couldn't parse tamplate", err, funcName)
-	}
-	http.Handle("/ws/", middleware.AuthMiddleware(http.HandlerFunc(chat)))
-	http.Handle("/chat/", middleware.AuthMiddleware(http.HandlerFunc(chatTemplate)))
-	http.Handle("/delete/", middleware.AuthMiddleware(http.HandlerFunc(deleteMessage)))
-	http.HandleFunc("/login/", loginHandler().ServeHTTP)
-	http.HandleFunc("/sign-up/", signUpHandler().ServeHTTP)
+	http.Handle("/ws/", middleware.AuthMiddleware(http.HandlerFunc(socket.Chat)))
+	http.Handle("/chat/", middleware.AuthMiddleware(http.HandlerFunc(socket.ChatTemplate)))
+	http.Handle("/delete/", middleware.AuthMiddleware(http.HandlerFunc(socket.DeleteMessage)))
+	http.HandleFunc("/login/", userHandler.LoginHandler)
+	http.HandleFunc("/sign-up/", userHandler.SignUpHandler)
+	http.HandleFunc("/logout/", userHandler.LogoutHandler)
 
 	err = http.ListenAndServe(":9090", nil)
 	if err != nil {
