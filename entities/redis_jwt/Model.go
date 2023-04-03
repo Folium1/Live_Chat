2a9137c204b3d0f1c22e9b670cb1eaed288de325
Folium1 <vampirowitch.vampir@gmail.com>
2023-04-03 16:11:03 +0300

    Refactored some layers

--- entities/redis_jwt/Model.go
+++
@@ -1,7 +0,0 @@
-package redis_jwt
-
-type RedisData struct {
-	Id    string `json:"user_id"`
-	Key   string `json:"key"`
-	Token string `json:"jwt"`
-}
--- entity/user/Model.go
+++
@@ -1,9 +0,0 @@
-package user
-
-type user struct {
-	Id       string `json:"id"`
-	Name     string `json:"name"`
-	Mail     string `json:"mail"`
-	Password string `json:"pass"`
-	Online   bool   `json:"online"`
-}
